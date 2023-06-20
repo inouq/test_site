2ab2a967065a38209078e1665aaac2d0d4f96f61
Shaldy <potasofka@gmail.com>
2023-06-20 17:52:33 +0500

    one

--- insert.php
+++ insert.php
@@ -17,6 +17,8 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_record'])){
   $service_type = '';
   $total_price = '';
 
+//
+
   if ($_POST['tariff'] === "Тариф 'Гость'") {
     if ($_POST['duration'] === "4 часа") {
       $tariff = '1';
