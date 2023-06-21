4f244bf189e4864805b52d378eaf4753becde45e
inouq <inouqq@gmail.com>
2023-06-21 19:08:36 +0500

    19^08

--- orders.php
+++ orders.php
@@ -1,7 +1,6 @@
 <?php 
 include 'landing.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,32 +9,45 @@ include 'landing.php';
     <link rel="stylesheet" type="text/css" href="css/orders.css">
 </head>
 <body>
-
   <h1>Список заказов</h1>
   <table>
     <thead>
       <tr>
-        <th>№ Заказа</th>
-        <th>Дата заказа</th>
-        <th>Статус</th>
+        <th>id Заказа</th>
+        <th>Имя</th>
+        <th>Фамилия</th>
+        <th>Отчество</th>
+        <th>E-mail</th>
+        <th>Номер телефона</th>
+        <th>Вид деятельности</th>
+        <th>id Тарифа</th>
+        <th>Наименование тарифа</th>
+        <th>info_id</th>
+        <th>Длительность</th>
+        <th>Итого</th>
       </tr>
     </thead>
-    <tbody>
-      <tr>
-        <td>001</td>
-        <td>01.01.2022</td>
-        <td>Ожидает обработки</td>
-      </tr>
-      <tr>
-        <td>002</td>
-        <td>02.01.2022</td>
-        <td>Обработка завершена</td>
-      </tr>
-      <tr>
-        <td>003</td>
-        <td>03.01.2022</td>
-        <td>Отправлено</td>
-      </tr>
+    <tbody name="table">
+      <?php 
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo "<tr class='table_length'>";
+            echo  ('<td>'. '<input class= table_length_id value = '. $row["id"] . '>'  . '</td>');  
+            echo  ('<td>'. '<input class= table_length value = '. $row["first_name"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length value = '. $row["last_name"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length value = '. $row["middle_name"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length value = '. $row["email"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length value = '. $row["phone_number"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length value = '. $row["activity"] . '>' . '</td>');
+            echo  ('<td>'. '<input class= table_length_id value = '. $row["tariff_id"] . '>' . '</td>');
+            echo ('<td>' . '</td>');
+            echo  ('<td>'. '<input class= table_length_id value = '. $row["info_id"] . '>' . '</td>');
+            echo ('<td>' . '</td>');
+            echo  ('<td>'. '<input class= table_length_price value = '. $row["total_price"] . '>' . '</td>');
+            echo "</tr>";
+          } 
+        }
+      ?>
     </tbody>
   </table>
 </body>
