4b3a4e4f9422c0ba58e5c36499e950c57fb60a79
Shaldue <90022270+Shaldue@users.noreply.github.com>
2023-06-21 19:08:28 +0500

    recode

--- index.php
+++ index.php
@@ -23,54 +23,59 @@ include 'insert.php';
                     <option id ="conference-hall" value="conference-hall">Конференц-зал</option>
                     <option id ="meeting-room" value="meeting-room">Переговорка</option>
                 </select><br>
+
                 <div class="pole" id="tariff_workspace">
                     <div class="tariffs"><h1 class ='tariffs_2'>Тариф "Гость"</h1>
                         <div class="buttons">
-                            <button id="guest_4h" type="button" name="">4 часа</button>
-                            <button id="guest_1d" type="button" name="">1 день</button>
-                            <button id="guest_1m" type="button" name="">1 месяц</button>
-                            <button id="guest_3m" type="button" name="">3 месяца</button>
+                            <button class = "tariffs_button" id="guest_4h" type="button" name="guest_4h">4 часа</button>
+                            <button class = "tariffs_button" id="guest_1d" type="button" name="guest_1d">1 день</button>
+                            <button class = "tariffs_button" id="guest_1m" type="button" name="guest_1m">1 месяц</button>
+                            <button class = "tariffs_button" id="guest_3m" type="button" name="guest_3m">3 месяца</button>
                         </div>
                     </div>
+
                     <div class="tariffs"><h1 class ='tariffs_2'>Тариф "Резидент"</h1>
                         <div class="buttons">
-                            <button id="resident_1m" type="button" name="">1 месяц</button>
-                            <button id="resident_3m" type="button" name="">3 месяца</button>
+                            <button class = "tariffs_button" id="resident_1m" type="button" name="resident_1m">1 месяц</button>
+                            <button class = "tariffs_button" id="resident_3m" type="button" name="resident_3m">3 месяца</button>
                         </div>
                     </div>
+
                     <div class="tariffs"><h1 class ='tariffs_2'>Тариф "Агентство"</h1>
                         <div class="buttons">
-                            <button id="agency" type="button" name="">Выбрать</button>
+                            <button class = "tariffs_button" id="agency" type="button" name="agency">Выбрать</button>
                         </div>
-                    </div>
-                    
+                    </div> 
                 </div>
+                
                 <div class = "pole" id="tariff_conference-hall">
                     <div class="tariffs"><h1 class ='tariffs_2'>Конференц-зал</h1>
-                    <div class ='tariffs_2'>1 час = 500 руб.</div>
+                        <div class ='tariffs_2'>1 час = 500 руб.</div>
                         <select class="notification" id="meeting_room" name="meeting_room">
-                        <option value="" selected disabled hidden>Срок аренды</option>
-                        <option id ="">1</option>
-                        <option id ="">2</option>
-                        <option id ="">3</option>
-                        <option id ="">4</option>
-                        <option id ="">5</option>
-                        <option id ="">6</option>
-                        <option id ="">7</option>
-                        <option id ="">8</option>
+                            <option value="" selected disabled hidden>Срок аренды</option>
+                            <option id ="">1</option>
+                            <option id ="">2</option>
+                            <option id ="">3</option>
+                            <option id ="">4</option>
+                            <option id ="">5</option>
+                            <option id ="">6</option>
+                            <option id ="">7</option>
+                            <option id ="">8</option>
                         </select><br>
                     </div>
                 </div>
-            </div>  
+            </div> 
+
             <div class = "pole" id="tariff_meeting-room">
-            <div class="tariffs"><h1 class ='tariffs_2'>Переговорка</h1>
-                        <div class="buttons">
-                            <button id="conf_room1h" type="button" name="">1 час</button>
-                            <button id="conf_room4h" type="button" name="">Пол дня (4 часа)</button>
-                            <button id="conf_room1d" type="button" name="">День</button>
-                        </div>
+                <div class="tariffs"><h1 class ='tariffs_2'>Переговорка</h1>
+                    <div class="buttons">
+                        <button class = "tariffs_button" id="conf_room1h" type="button" name="conf_room1h">1 час</button>
+                        <button class = "tariffs_button" id="conf_room4h" type="button" name="conf_room1h">Пол дня (4 часа)</button>
+                        <button class = "tariffs_button" id="conf_room1d" type="button" name="conf_room1h">День</button>
                     </div>
+                </div>
             </div>
+
             <div><h4>Ваш выбор: </h4></div>
             <div id="hidden_div" class="hidden_field">
                 <input name="tariff" id="hidden_f" type="text" value="Ваш тариф">
--- js/script.js
+++ js/script.js
@@ -5,25 +5,86 @@ var divBox = document.getElementById("tariff_workspace");
 var divBox2 = document.getElementById("tariff_conference-hall");
 var divBox3 = document.getElementById("tariff_meeting-room");
 
-var guest_4h = document.getElementById("guest_4h");
-var guest_1d = document.getElementById("guest_1d");
-var guest_1m = document.getElementById("guest_1m");
-var guest_3m = document.getElementById("guest_3m");
-var resident_1m = document.getElementById("resident_1m");
-var resident_3m = document.getElementById("resident_3m");
-var agency = document.getElementById("agency");
-var conf_room1h = document.getElementById("conf_room1h");
-var conf_room4h = document.getElementById("conf_room4h");
-var conf_room1d = document.getElementById("conf_room1d");
-
 var hidden_f = document.getElementById("hidden_f");
 var hidden_f2 = document.getElementById("hidden_f2");
 var hidden_div = document.getElementById("hidden_div");
-var price = document.getElementById("price");
+var total_price = document.getElementById("price");
 
 var emailInput = document.getElementById("email");
 
-varform = document.getElementById("form");
+var form = document.getElementById("form");
+
+//Получение объектов кнопок
+const buttons = document.querySelectorAll(".tariffs_button");
+
+for (let button of buttons) {
+  button.addEventListener("click", function() {
+    setTariff(button.name);
+  })
+}
+
+//Установка выбранного тарифа в поля информации.
+function setInfo(tariffName, duration, price) {
+  hidden_f.value = tariffName;
+  hidden_f2.value = duration;
+  total_price.innerHTML = "<h3><b>" + price + " руб.</b></h3>";
+  return 0;
+}
+
+function setTariff(buttonName) {
+
+  hidden_div.style.display = "block";
+  myButton.style.backgroundColor = '';
+  myButton.disabled = false;
+
+  switch (buttonName) {
+    case "guest_4h":
+      setInfo("Тариф 'Гость'", "4 часа", 300);
+      break;
+
+    case "guest_1d":
+      setInfo("Тариф 'Гость'", "1 день", 400);
+      break;
+
+    case "guest_1m":
+      setInfo("Тариф 'Гость'", "1 месяц", 3500);
+      break;
+
+    case "guest_3m":
+      setInfo("Тариф 'Гость'", "3 месяца", 9000);
+      break;
+    
+    case "agency":
+      setInfo("Тариф 'Агентство'", "-", 0);
+      break;
+    
+    case "resident_1m":
+      setInfo("Тариф 'Резидент'", "1 месяц", 4500);
+      break;
+
+    case "resident_3m":
+      setInfo("Тариф 'Резидент'", "3 месяца", 12000);
+      break;
+
+    case "conf_room1h":
+      setInfo("Переговорка", "1 час", 200);
+      break;
+
+    case "conf_room4h":
+      setInfo("Переговорка", "Пол дня (4 часа)", 500);
+      break;
+
+    case "conf_room1d":
+      setInfo("Переговорка", "День", 900);
+      break;
+
+    default:
+      hidden_div.style.display = "hide";
+      myButton.style.backgroundColor = 'gray';
+      myButton.disabled = true;
+      alert("Ошибка выбора тарифа! Обратитесь к разработчику!");
+  }
+}
 
 // function validateEmail(email) {
 //   const re = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
@@ -95,136 +156,3 @@ selectBox.addEventListener("change", function() {
     divBox2.style.display = "none";
   }
 });
-
-meeting_room.addEventListener("change", function() {
-  var selectedValue = meeting_room.options[meeting_room.selectedIndex].value;
-
-  hidden_div.style.display = "block";
-  hidden_f.value = "'Конференц-Зал'";
-  hidden_f2.value = selectedValue + " часа";
-  price.innerHTML = "<h3><b>" + selectedValue * 500 + " руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-guest_4h.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Гость'";
-  hidden_f2.value = "4 часа";
-  price.innerHTML = "<h3><b>300 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-guest_1d.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Гость'";
-  hidden_f2.value = "1 день";
-  price.innerHTML = "<h3><b>400 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-guest_1m.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Гость'";
-  hidden_f2.value = "1 месяц";
-  price.innerHTML = "<h3><b>3500 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-guest_3m.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Гость'";
-  hidden_f2.value = "3 месяца";
-  price.innerHTML = "<h3><b>9000 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-// резидент
-resident_1m.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Резидент'";
-  hidden_f2.value = "1 месяц";
-  price.innerHTML = "<h3><b>4500 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-resident_3m.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Резидент'";
-  hidden_f2.value = "3 месяца";
-  price.innerHTML = "<h3><b>12000 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-agency.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Тариф 'Агентство'";
-  hidden_f2.value = "-";
-  price.innerHTML = "<h3><b>-</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-// переговорка
-conf_room1h.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Переговорка";
-  hidden_f2.value = "1 час";
-  price.innerHTML = "<h3><b>200 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-conf_room4h.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Переговорка";
-  hidden_f2.value = "Пол дня (4 часа)";
-  price.innerHTML = "<h3><b>500 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-conf_room1d.addEventListener("click", function() {
-  hidden_div.style.display = "block";
-  hidden_f.value = "Переговорка";
-  hidden_f2.value = "День";
-  price.innerHTML = "<h3><b>900 руб.</b></h3>";
-  myButton.style.backgroundColor = '';
-  myButton.disabled = false;
-});
-
-// emailInput.addEventListener("change", function() {
-//   const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
-//   var email_value = emailInput.value;
-//   if (emailRegex.test(email_value)) {
-//   } else {
-//     const message = 'Введите корректный e-mail адрес';
-//     emailInput.setCustomValidity(message);
-//     emailInput.reportValidity();
-//     // alert('Номер телефона некорректен:', phone_value);
-//   }
-// });
-
-// phone_input.addEventListener("change", function() {
-//   const phoneRegex = /^\+7\s?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{2}[-.\s]?\d{2}$/;
-//   var phone_value = phone_input.value;
-//   if (phoneRegex.test(phone_value)) {
-//     form.submit();
-//   } else {
-//     const message = 'Введите корректный номер телефона';
-//     phone_input.setCustomValidity(message);
-//     phone_input.reportValidity();
-//     // alert('Номер телефона некорректен:', phone_value);
-//   }
-// });
-
-// function isValidEmail(email) {
-//   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   return emailRegex.test(email);
-// }
